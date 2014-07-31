1174a447a22bc68961c97bc69a48bfed40932a8b
pchatterton <pachatterton@gmail.com>
2014-07-30 23:47:49 -0600

    make minor changes

--- app/scripts/controllers/registrants.js
+++ app/scripts/controllers/registrants.js
@@ -2,11 +2,17 @@
 
 var app = angular.module('rafflePrizeApp');
 
-app.controller('RegistrantsCtrl', function ($scope) {
+app.controller('RegistrantsCtrl', function ($scope, Registrants) {
 
 	$scope.test = 'test'
 
 	$scope.uploadEmails = function(text) {
 		console.log('text: ' + text)
+		Registrants.verifyText(text)
+		var successRegistrants = Registrants.getRegistrants();
+		var failedRegistrants = Registrants.getFailedUpload();
+
+		console.log(successRegistrants)
+		console.log(failedRegistrants)
 	}
 });
--- app/scripts/services/registrants.js
+++ app/scripts/services/registrants.js
@@ -3,15 +3,39 @@
 var app = angular.module('rafflePrizeApp');
 
 app.factory('Registrants', function () {
-    // Service logic
-    // ...
 
-    var meaningOfLife = 42;
+   var registrantsService = {};
+   var newRegistrants = [];
+   var failedUpload = [];
 
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
-  });
+
+    registrantsService.verifyText = function(text) {
+    	var tempRegistrants = []
+    	tempRegistrants = text.split(';');
+    	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    	for(var i=0; i<tempRegistrants.length; i++) {
+    		tempRegistrants[i] = tempRegistrants[i].replace(/ /g,'')
+    		var result = re.test(tempRegistrants[i])
+    		if(result) {
+    			newRegistrants.push(tempRegistrants[i])
+    		} else {
+    			if (!(tempRegistrants[i] === '')) {
+    				failedUpload.push(tempRegistrants[i])
+    			};
+    		}
+    	}
+    }
+
+
+    registrantsService.getRegistrants = function() {
+    	return newRegistrants;
+    }
+
+    registrantsService.getFailedUpload = function() {
+    	return failedUpload;
+    }
+
+    return registrantsService;
+
+
+  }); //Close Factory
